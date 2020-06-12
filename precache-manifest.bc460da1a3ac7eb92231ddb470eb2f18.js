40de77154c5c0c33864ad3d0cde6cfae32d87c03
Fishbakh-N <nikita.fishbakh.work@gmail.com>
2020-06-12 18:39:56 +0300

    Updates

---
+++ precache-manifest.bc460da1a3ac7eb92231ddb470eb2f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dbedf2f7d8edeadecfcc4efe8bacd9",
+    "url": "/test-login-form-react/index.html"
+  },
+  {
+    "revision": "e035c843d6a77306a299",
+    "url": "/test-login-form-react/static/css/main.b6ded675.chunk.css"
+  },
+  {
+    "revision": "672f51f773ad5f9eee92",
+    "url": "/test-login-form-react/static/js/2.88ca89cb.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/test-login-form-react/static/js/2.88ca89cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e035c843d6a77306a299",
+    "url": "/test-login-form-react/static/js/main.508825c8.chunk.js"
+  },
+  {
+    "revision": "5092978edf2b32805c5d",
+    "url": "/test-login-form-react/static/js/runtime-main.889d530b.js"
+  },
+  {
+    "revision": "90fa2009b5444e7332857092941a0008",
+    "url": "/test-login-form-react/static/media/bg.90fa2009.png"
+  }
+]);
